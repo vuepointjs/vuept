8c1c7f7ad3b708353ad20181adce8859386f0ff3
SLathrop <steve@stela.io>
2018-07-31 14:52:02 -0400

    WIP add suite script

--- scripts/add-suite.js
+++ scripts/add-suite.js
@@ -1,25 +1,46 @@
 /*
   add-suite.js
 
-    TODO: Implement script that adds a new "suite" to the solution by accepting a suite key string argument and:
-    
-      - Verifying that the suite key is valid (of the right form) and that the specified suite doesn't already exist
+    TODO: Implement script that adds a new "suite" to the solution by prompting for required inputs and:
+
+      - Verifying that the inputs (e.g., suite key) are valid and that the specified suite doesn't already exist
       - Copying .templates/suite to solution/suite/{{suite-key}}
-      - Adding (if necessary) the suite to the json data in /solution/data/Tenants/Suites.json
+      - Adding (if necessary) the suite to the json data in /solution/data/tenants/suites.json
 */
 
-const suiteKey = process.argv[2];
-const suites = require('@vuept/solution-data').suites;
+// const suiteKey = process.argv[2];
+// const suites = require('@vuept/solution-data').suites;
 
-if (!suiteKey) {
-  console.log('\n\nadd:suite - Missing required <suite-key> arg');
-  console.log('Format: npm run add:suite "<suite-key>"');
-  process.exit(1);
-}
+// if (!suiteKey) {
+//   console.log('\n\nadd:suite - Missing required <suite-key> arg');
+//   console.log('Format: npm run add:suite "<suite-key>"');
+//   process.exit(1);
+// }
 
-console.log();
-console.log(`add-suite: Under construction. Soon your specified suite "${suiteKey}" will be added to the solution.`);
-console.log(`           Suite count: ${suites.length}\n`);
+// console.log();
+// console.log(`add-suite: Under construction. Soon your specified suite "${suiteKey}" will be added to the solution.`);
+// console.log(`           Suite count: ${suites.length}\n`);
 
-// Main flow of execution for script ends here
-process.exit(0);
+// // Main flow of execution for script ends here
+// process.exit(0);
+
+const schema = require('@vuept/solution-admin').schema;
+
+console.log('\nsuite schema...');
+console.log(schema.suite);
+
+console.log('\napp schema...');
+console.log(schema.app);
+
+console.log('\napplet schema...');
+console.log(schema.applet);
+
+console.log('\nview schema...');
+console.log(schema.view);
+
+const inquirer = require('inquirer');
+const questions = [];
+
+// inquirer.prompt(questions).then(answers => {
+//   console.log(answers);
+// });
---
+++ solution/admin/index.js
@@ -0,0 +1,51 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+
+let schema = {
+  suite: null,
+  app: null,
+  applet: null,
+  view: null
+};
+
+// We use Netlify CMS config.yml spec as source of truth and parse individual schemas from it
+try {
+  const config = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, './config.yml'), 'utf8'));
+  const schemaContainer = config.collections[0].files[0].fields[0];
+
+  schema.suite = _(schemaContainer.fields)
+    .filter(val => val.name != 'apps')
+    .value();
+
+  const appFields = _.chain(schemaContainer.fields)
+    .filter(['name', 'apps'])
+    .first()
+    .get('fields')
+    .value();
+  schema.app = _(appFields)
+    .filter(val => val.name != 'applets')
+    .value();
+
+  const appletFields = _.chain(appFields)
+    .filter(['name', 'applets'])
+    .first()
+    .get('fields')
+    .value();
+  schema.applet = _(appletFields)
+    .filter(val => val.name != 'views')
+    .value();
+
+  schema.view = _.chain(appletFields)
+    .filter(['name', 'views'])
+    .first()
+    .get('fields')
+    .value();
+
+  // console.log(suiteSchema);
+} catch (e) {
+  // console.log(e);
+}
+
+module.exports.schema = schema;
