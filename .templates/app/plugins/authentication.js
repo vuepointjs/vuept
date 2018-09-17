6167b5fc8a2864b65e1c77d4ae80b719f3010bdf
SLathrop <steve@stela.io>
2018-09-17 13:56:32 -0400

    WIP jwt auth w/ adal

--- .templates/app/plugins/authentication.js
+++ .templates/app/plugins/authentication.js
@@ -3,35 +3,26 @@ import authentication from '@vuept/ui/authentication';
 import _ from 'lodash';
 
 const vpCtx = process.env.vpCtx;
-const isNodeDev = vpCtx.isNodeDev;
-const isTemplateDev = vpCtx.isTemplateDev;
+const useFakeAuth = vpCtx.isTemplateDev;
+const azureProfileKey = vpCtx.isNodeDev ? 'DEV' : 'PROD';
+const azureData = _(vpCtx.suiteData.azure)
+  .filter({ key: azureProfileKey })
+  .first();
 
 // console.log('In "authentication" plugin...');
 // console.log(`vpCtx: ${JSON.stringify(vpCtx, null, 2)}`);
+// console.log(`suiteData: ${JSON.stringify(suiteData, null, 2)}`);
+// console.log(`azureData: ${JSON.stringify(azureData, null, 2)}`);
 
-if (isTemplateDev) {
-  console.log('Loading "authentication" plugin... skipping auth for template development');
-} else {
-  const suiteData = vpCtx.suiteData;
-  // console.log(`In "authentication" plugin suiteData is ${JSON.stringify(suiteData, null, 2)}`);
+// Init authentication module
+authentication.initialize(azureData, useFakeAuth, vpCtx.isVerbose).then(_ => {
+  // Install "auth" as a Vue plugin
+  const authenticationPlugin = {
+    install() {
+      Vue.auth = authentication;
+      Vue.prototype.$auth = authentication;
+    }
+  };
 
-  const azureProfileKey = vpCtx.isNodeDev ? 'DEV' : 'PROD';
-  const azureData = _(suiteData.azure)
-    .filter({ key: azureProfileKey })
-    .first();
-
-  // console.log(`In "authentication" plugin azureData is ${JSON.stringify(azureData)}`);
-
-  // Init authentication module
-  authentication.initialize(azureData).then(_ => {
-    // Install "authentication" as a Vue plugin
-    const authenticationPlugin = {
-      install() {
-        Vue.authentication = authentication;
-        Vue.prototype.$authentication = authentication;
-      }
-    };
-
-    Vue.use(authenticationPlugin);
-  });
-}
+  Vue.use(authenticationPlugin);
+});
