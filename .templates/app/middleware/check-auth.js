892c51ed04de71d8b54a8daa4a5ab65bb11bc110
SLathrop <steve@stela.io>
2018-09-18 17:10:47 -0400

    WIP refactor ui plugins + middleware

--- .templates/app/middleware/check-auth.js
+++ .templates/app/middleware/check-auth.js
@@ -1,7 +1,10 @@
+/**
+ * check-auth.js: VP global middleware (all routes and pages) to check for user auth
+ */
 export default function(context) {
   console.log('MW: check-auth');
 
-  // let isAuthenticated = context.app.$auth.isAuthenticated();
+  let isAuthenticated = context.app.$auth.isAuthenticated();
   // console.log(context, null, 2);
 
   // const loggedUser = isServer ? getUserFromCookie(req) : getUserFromLocalStorage();
