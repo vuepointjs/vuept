543aa2018d4d6526e197f3a1d3bd97047a452192
SLathrop <steve@stela.io>
2018-09-20 17:39:52 -0400

    Cleanup minor DevOps and auth details

--- .templates/app/layouts/default.vue
+++ .templates/app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app light v-if="$auth.isTenantUser()">
+  <v-app light>
     <suite-bar @waffle-click="drawer = !drawer" />
 
     <v-navigation-drawer temporary fixed light v-model="drawer" width="320">
